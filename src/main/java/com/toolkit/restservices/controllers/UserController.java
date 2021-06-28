8c0a9879975ca968f510f993c4ac088a8f9c606b
Raj Jain <adhiraj.rawka@gmail.com>
2021-06-28 11:31:54 +0530

    Commit

--- src/main/java/com/toolkit/restservices/controllers/UserController.java
+++ src/main/java/com/toolkit/restservices/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.toolkit.restservices.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +33,16 @@ public class UserController {
 		return userService.createUser(user);
 	}
 	
+	
+	//updateUserById
+		@PutMapping("/users/{id}")
+		public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
+			return userService.updateUserById(id, user);
+		}
+	
+		//deleteUserById
+		@DeleteMapping("/users/{id}")
+		public void deleteUserById(@PathVariable("id") Long id) {
+			userService.deleteUserById(id);
+		} 
 }
--- src/main/java/com/toolkit/restservices/services/UserService.java
+++ src/main/java/com/toolkit/restservices/services/UserService.java
@@ -1,6 +1,7 @@
 package com.toolkit.restservices.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,4 +26,28 @@ public class UserService {
 		return userRepository.save(user);
 	}
 
+	
+	
+	public Optional<User> getUserById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		return user;
+	}
+	
+	
+	public User updateUserById(Long id, User user) {
+		user.setId(id);
+		return userRepository.save(user);
+		
+	}
+	
+	
+	public void deleteUserById(Long id) {
+		if(userRepository.findById(id).isPresent()) {
+			userRepository.deleteById(id);
+			
+		}
+	
+}
+	
 }
